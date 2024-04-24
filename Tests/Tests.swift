d471fc7c65e1a1ae653adc0c21b8572eb57d2b72
Alex Khuala <khuala.dev@gmail.com>
2024-04-24 15:31:49 +0200

    Configured project; prepared test names

---
+++ Tests/Tests.swift
@@ -0,0 +1,37 @@
+//
+//  Tests.swift
+//  Tests
+//
+//  Created by Alex Khuala on 24.04.24.
+//
+
+import XCTest
+
+final class Tests: XCTestCase 
+{
+
+    override func setUpWithError() throws 
+    {
+        
+    }
+
+    override func tearDownWithError() throws 
+    {
+        
+    }
+
+    func testTreeParser()
+    {
+        
+    }
+    
+    func testDetailsParser()
+    {
+        
+    }
+    
+    func testRemoteTreeDataParser()
+    {
+        
+    }
+}
