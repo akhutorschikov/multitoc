e9ef9c037d803057c715fe3ba86ef7d8aec1d58d
Alex Khuala <khuala.dev@gmail.com>
2024-04-24 15:23:15 +0200

    Initial Commit

---
+++ homework-img.ly/homework_img_lyApp.swift
@@ -0,0 +1,17 @@
+//
+//  homework_img_lyApp.swift
+//  homework-img.ly
+//
+//  Created by Alex Khuala on 24.04.24.
+//
+
+import SwiftUI
+
+@main
+struct homework_img_lyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
