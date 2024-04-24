5c28cebdc10aaed4443e379255f496e3699e2327
Alex Khuala <khuala.dev@gmail.com>
2024-04-24 15:27:50 +0200

    Created basic project structure

--- homework-img.ly/View/HomeView.swift
+++ homework-img.ly/View/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  homework-img.ly
 //
 //  Created by Alex Khuala on 24.04.24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    HomeView()
 }
