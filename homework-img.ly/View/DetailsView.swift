4b09ad21e42d75b3d755c986fc330a59608dc695
Alex Khuala <khuala.dev@gmail.com>
2024-04-24 16:51:54 +0200

    Implemented home view

---
+++ homework-img.ly/View/DetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailsView.swift
+//  homework-img.ly
+//
+//  Created by Alex Khuala on 24.04.24.
+//
+
+import SwiftUI
+
+struct DetailsView: View 
+{
+    let id: String
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    DetailsView(id: "id_0")
+}
