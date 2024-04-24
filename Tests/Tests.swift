6612162be8d04c4289f7c59277edd1b9fb05a204
Alex Khuala <khuala.dev@gmail.com>
2024-04-24 15:55:13 +0200

    Added model, local sample data, implemented unit tests

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -6,32 +6,77 @@
 //
 
 import XCTest
+@testable import homework_img_ly
 
 final class Tests: XCTestCase 
 {
+    var sut: KHContentManager!
+    let networkMonitor = KHNetworkMonitor.shared
 
     override func setUpWithError() throws 
     {
-        
+        try super.setUpWithError()
+        self.sut = KHContentManager.shared
     }
 
     override func tearDownWithError() throws 
     {
-        
+        self.sut = nil
+        try super.tearDownWithError()
     }
 
-    func testTreeParser()
+    func testTreeParser() throws
     {
+        XCTAssert(self.sut.entries.isEmpty)
+     
+        // given
+        let promise = expectation(description: "Completion handler invoked")
+        self.sut.local = true
         
+        // when
+        self.sut.loadTree {
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 3)
+        
+        // then
+        XCTAssertFalse(self.sut.entries.isEmpty)
     }
     
-    func testDetailsParser()
+    func testDetailsParser() throws
     {
+        // given
+        let promise = expectation(description: "Completion handler invoked")
+        self.sut.local = true
+        var entry: KHDetailsEntry?
         
+        // when
+        self.sut.loadDetails("id_1") { result in
+            entry = result
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 3)
+        
+        // then
+        XCTAssertNotNil(entry)
     }
     
-    func testRemoteTreeDataParser()
+    func testRemoteTreeDataParser() throws
     {
+        try XCTSkipUnless(self.networkMonitor.isReachable, "Network connectivity needed for this test.")
+        XCTAssert(self.sut.entries.isEmpty)
         
+        // given
+        let promise = expectation(description: "Completion handler invoked")
+        self.sut.local = false
+        
+        // when
+        self.sut.loadTree {
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 3)
+        
+        // then
+        XCTAssertFalse(self.sut.entries.isEmpty)
     }
 }
